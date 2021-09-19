4896fd0629c32b7138ff985c7394725dc844b62f
Shubham6898 <shubham.setia.3701@gmail.com>
2021-09-19 12:57:57 +0530

    Fixed new_file.py using shubham_work branch

--- new_file.py
+++ new_file.py
@@ -1,2 +1,7 @@
 print("Hello World")
 print("bhai git sikh rha hoon completely")
+print("AAB thoda shubham_work branch par kam kar rha hoon mai")
+print("Phir mai shubham_work branch ko main branch ke sath merge kar duga")
+''' one of the most dominant feature of git is making diff branch as per your requirement and then merging it with 
+other or main branch 
+'''
